5da865e6953b03ba5f434982cb593edb9b0b8763
gnaghiyeva <gulnar.nagiyeva17@gmail.com>
2023-02-26 21:37:44 +0400

    task1 with c#

---
+++ Task1-C#/if-else-task1-C#/if-else-task1-C#/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+double mebleg = 98;
+double result;
+
+if (mebleg < 100)
+{
+
+    result = mebleg - (mebleg * 0.05);
+    Console.WriteLine("5%-li endirim:" + " " + result);
+}
+
+else if (mebleg >= 100 && mebleg <= 1000)
+{
+    result = mebleg - (mebleg * 0.08);
+    Console.WriteLine("8%-li endirim:" + " " + result);
+}
+
+else
+{
+    result = mebleg - (mebleg * 0.10);
+    Console.WriteLine("10%-li endirim:" + " " + result);
+}
